ff8b8fe0ad31299bdc8a18acef0dd5cb0aaf00dd
Desisacc <saccescuela@gmail.com>
2024-02-21 08:47:10 -0700

    Implemented invalid input
    Now when it receives an input that mathjs cannot handle it will inform that it could not evaluate the formula rather than throwing an exception or saying it's incorrect.

--- Server/formateo.js
+++ Server/formateo.js
@@ -1,18 +1,6 @@
-export function FormatearParaURL(formula)
+export async function Simplificar(formula)
 {
-    let formulaFormateada = formula.replace('/', '(div)');
-    return formulaFormateada;
-}
-
-export function FormatearDeURL(formulaDeURL)
-{
-    let formulaFormateada = formulaDeURL.replace('(div)', '/');
-    return formulaFormateada;
-}
-
-export async function Evaluar(formula)
-{
-    let formulaFormateada = FormatearParaURL(formula);
+    let formulaFormateada = encodeURIComponent(formula);
     let resultado = await fetch('/Evaluate/' + formulaFormateada).then(res => res.json()).then(json => json.resultado);
     return resultado;
 }
